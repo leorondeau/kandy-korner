d78aa64bdb2df15eb9f3a9f439603fd026924722
admin <christopherleorondeau@gmail.com>
2020-11-28 18:00:15 -0600

    added products to db and rendered them on the dom through provide and list modules

---
+++ src/components/product/ProductProvider.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react"
+import { Product } from "./Product"
+
+export const ProductContext = React.createContext()
+
+export const ProductProvider = (props) => {
+    const [products, setProducts] = useState([])
+
+    const getProducts = () => {
+        return fetch ("http://localhost:8088/products")
+        .then(res => res.json())
+        .then(setProducts)
+    }
+
+    const addProduct = location => {
+        return fetch ("http://localhost:8088/products" , {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(location)
+    })
+    .then(getProducts)
+}
+
+    return (
+        <ProductContext.Provider value = {{
+            products , addProduct , getProducts
+        }}>
+            {props.children}
+        </ProductContext.Provider>
+    )
+}
