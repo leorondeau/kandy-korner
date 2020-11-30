b30d621c07a2b3fd775a7b4d72a1ff164215100c
admin <christopherleorondeau@gmail.com>
2020-11-30 11:12:49 -0600

    added product to productType

--- src/components/product/ProductList.js
+++ src/components/product/ProductList.js
@@ -2,13 +2,23 @@ import React, { useContext , useEffect } from "react"
 import { ProductContext } from "./ProductProvider"
 import { Product } from "./Product"
 import "./Product.css"
+import { ProductTypeContext } from "./ProductTypeProvider"
+
+// console.log("ProductContext" , ProductContext)
+// console.log("ProductTypeContext" , ProductTypeContext)
 
 export const ProductList = () => {
-
+    
     const { products , getProducts } = useContext(ProductContext)
-
+    const { productTypes , getProductTypes} = useContext(ProductTypeContext)
+    // console.log("proTYPE" , productType)
+    // console.log("ProductContext in ProductList" , ProductContext)
+    // console.log("useContext in ProductList" , useContext(ProductContext))
+    // console.log("products in ProductList" , products)
+    // console.log("getProducts in productList" , getProducts)
     useEffect(() => {
-        getProducts()
+        getProductTypes()
+        .then(getProducts)
     }, [])
 
     useEffect(() => { 
@@ -17,9 +27,18 @@ export const ProductList = () => {
 
     return (
         <div className="products">
-            {
-                products.map(pro => <Product key={pro.id} product={pro} />)
-            }
+            {/* {console.log("products/productTypes" , products, productTypes)} */}
+            {   
+                products.map(pro => {
+                    const productTypeObj = productTypes.find(pt => pro.productTypeId === pt.id)  
+                    console.log("productTypeObj" , productTypeObj)
+                // debugger
+               return <Product key={pro.id}  
+                               productType={productTypeObj}
+                               product={pro} /> 
+            })
+        }
         </div>
     )
 } 
+// console.log("pro in ProductList" , pro.id)
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
-// import { BrowserRouter as Router } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 import { KandyKorner } from "./components/KandyKorner"
 import "./index.css"
  
 
 ReactDOM.render(
     <React.StrictMode>
-      {/* <Router> */}
+      <Router>
          <KandyKorner />
-      {/* </Router> */}
+      </Router>
     </React.StrictMode>,
     document.getElementById('root')
   );
